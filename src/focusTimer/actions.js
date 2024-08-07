2cdd89b646796f33afa4f6ffe79a0c01eb7ca63b
Marco Pereira <marcope043@gmail.com>
2024-08-07 12:49:54 -0300

    search errors

--- src/focusTimer/actions.js
+++ src/focusTimer/actions.js
@@ -1,10 +1,34 @@
 import state from './state.js';
 import * as el from './elements.js';
 import * as timer from './timer.js'
+import * as sounds from './sounds.js'
+
+export function ToggleRunning(){
+  state.isRunning = document.documentElement.classList.toggle
+  ('running')
+
+  timer.countDown()
+  sounds.buttonPressAudio.play()
+}
 
 export function reset(){
   state.isRunning = false 
   document.documentElement.classList.remove('running')
   timer.updateDisplay()
   sounds.buttonPressAudio.play()
+}
+
+export function set(){
+  el.minutes.setAttribute('contenteditable', true)
+  el.minutes.focus
+}
+
+export function toggleMusic(){
+  state.isMute = document.documentElement.classList.toggle('musicON')
+  if(state.isMute) {
+    sounds.coffeeAudio.play()
+    return
+  }
+
+  sounds.coffeeAudio.pause()
 }
--- src/focusTimer/state.js
+++ src/focusTimer/state.js
@@ -2,4 +2,6 @@ export default {
   minutes: 25,
   seconds: 0,
   isRunning: false,
+  isMute: true,
+  countDownId: null
 }
